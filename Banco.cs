6bc5e850e94ee16eddc4fd267f7ddc45a7df90f7
rstringhini <rstringhini@inf.ufsm.br>
2018-06-04 11:32:46 -0300

    Add files via upload

---
+++ Banco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoBlabla
+{
+    public class Banco
+    {
+        public Banco()
+        {
+
+            //conta corrente(saldo, poupanca, limite, fatura, saque e deposito), funcionario(gerente, caixa e atendente), 
+            //cliente 
+
+        }
+
+
+
+    }
+}
---
+++ Gerente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoBlabla
+{
+    class Gerente : Pessoa
+    {
+
+        protected int cod;
+
+        public Gerente(int cod, string nome, int cpf, int identidade, string endereco) : base(nome, cpf, identidade, endereco)
+        {
+
+            this.cod = cod;
+
+        }
+    }
+}
